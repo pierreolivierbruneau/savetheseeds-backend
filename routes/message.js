a9b1a1916fca4ebae9a545a753094b14d8bee269
Kayla <kaylaljacks@hotmail.com>
2023-12-15 16:11:36 +0100

    messsage

--- routes/message.js
+++ routes/message.js
@@ -1,29 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const Message = require('../models/message');
+const Message = require("../models/message");
 
-router.post('/newmessage', (req, res) => {
+router.post("/newmessage", (req, res) => {
+  const newMessage = new Message({
+    title: req.body.title,
+    text: req.body.text,
+    username: req.body.username,
+  });
 
-        const newMessage = new Message({
-            title: req.body.title,
-            text: req.body.text,
-            username: req.body.username, 
-         });
+  newMessage.save().then((data) => {
+    console.log(data);
+    res.json({ result: true });
+  });
+});
 
-         newMessage.save().then(data => {
-            console.log(data)
-            res.json({result: true, message: data.message})
-         })
-    
-    });
-
-    router.get('/allmessages', function (req, res) {
-
-        Message.find().then(data => {
-            res.json({ result: true, message: data })
-        })
-    });
-
+router.get("/allmessages", function (req, res) {
+  Message.find().then((data) => {
+    res.json({ result: true, message: data });
+  });
+});
 
 module.exports = router;
